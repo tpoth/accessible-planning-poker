a2220e88fd14ba39b09725d5b418f0b3ac75a219
Thomas Poth <thomas.poth@gmail.com>
2022-07-03 15:05:36 +0200

    #6 #29 chang some word's to display
    on the homepage to make our tests fail.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /Rect App for Scrum Teams to play some rounds Planning Poker./i
+  );
   expect(linkElement).toBeInTheDocument();
 });
