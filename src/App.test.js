68be884a13db02befc9dde3c57a581cd62ea6da2
Thomas Poth <thomas.poth@gmail.com>
2022-07-03 15:08:54 +0200

    #6 #29 changed the name of the test
    to reflect the changed requirements we made before.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders 'Rect App for Scrum Teams to play some rounds Planning Poker.'", () => {
   render(<App />);
   const linkElement = screen.getByText(
     /Rect App for Scrum Teams to play some rounds Planning Poker./i
